4694a1e807c58f7d06f565f0445cfd53833b165f
João <joao@joao.codes>
2021-11-01 18:08:54 +0000

    lib directory

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,30 +1,8 @@
-<header>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/about">About Us</a></li>
-		</ul>
-	</nav>
-</header>
+<script>
+	import Header from '$lib/header.svelte';
+</script>
 
+<Header />
 <main>
 	<slot />
 </main>
-
-<style>
-	nav {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-	nav > ul {
-		display: flex;
-		flex-direction: row;
-	}
-	nav > ul > li {
-		display: block;
-		padding: 1rem;
-	}
-	main {
-	}
-</style>
