912390965b89d03c660862e9815505e58552e5ea
João <joao@joao.codes>
2021-11-02 14:55:48 +0000

    load data server side

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -1,1 +1,18 @@
-<h1>SLUG!</h1>
+<script context="module">
+	export async function load({ page }) {
+		const post = {
+			title: page.params.slug,
+			body: 'lorem ipsum dolor sit amet'
+		};
+		return {
+			props: { post }
+		};
+	}
+</script>
+
+<script>
+	export let post;
+</script>
+
+<h1 class="text-3xl">{post.title}</h1>
+<h4>{post.body}</h4>
