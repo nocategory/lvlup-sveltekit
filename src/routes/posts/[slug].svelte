9f09d2df14062c44145a936b210db6bab183807b
João <joao@joao.codes>
2021-11-06 17:31:42 +0000

    loading

--- src/routes/posts/[slug].svelte
+++ src/routes/posts/[slug].svelte
@@ -2,21 +2,26 @@
 	export async function load({ page }) {
 		// https://github.com/sveltejs/kit/issues/1326
 		// kinda broken, but this sort of works 👇
-		const Post = await import(`../../posts/${page.params.slug}.md`);
+		try {
+			const Post = await import(`../../posts/${page.params.slug}.md`);
 
-		return {
-			props: { Post: Post.default, title: Post.metadata.title }
-		};
+			return {
+				props: { Post: Post.default }
+			};
+		} catch (e) {
+			return {
+				status: 404,
+				error: 'NOT FOUND'
+			};
+		}
 	}
 </script>
 
 <script>
 	export let Post;
-	export let title;
 </script>
 
-<h3 class="text-2xl">Markdown:</h3>
+<h3>Post:</h3>
 
-{title}
-
-<Post />
+<!-- https://svelte.dev/docs#svelte_component -->
+<svelte:component this={Post} />
